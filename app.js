b506dda37219d7d938c79def0d89c849f0fbe8bb
Gyana Ranjan <Gyanaranjanmohanta491@gmail.com>
2025-01-05 20:32:56 +0530

    using port 3040

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3040;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
